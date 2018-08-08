2253670d6c8323093ff308e9c00a319f3d1de693
SculleyJr <sculley90@gmail.com>
2018-08-08 07:46:12 -0400

    first commit

--- Lab18.2/Program.cs
+++ Lab18.2/Program.cs
@@ -10,6 +10,37 @@ namespace Lab18._2
     {
         static void Main(string[] args)
         {
+            List<int> arrayList = new List<int>();
+            int[] array = {1, 2, 3, 2, 2, 4, 5, 5, 7 ,8 , 4, 4, 1, 0, 10};
+            int frequency = 0;
+
+            foreach (var num in array)
+            {
+                for (int n = 0; n <= array.Length-1; n++)
+                {
+                    if (arrayList.Contains(num))
+                    {
+                        break;
+                    }
+                    
+                    if (array[n] == num)
+                    {
+                        frequency++;
+                    }
+
+                   
+                }
+                arrayList.Add(num);
+                if (frequency == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine($"The number {num.ToString()} occurs {frequency} times");
+                frequency = 0;
+
+                Console.ReadKey();
+
+            }
         }
     }
 }
